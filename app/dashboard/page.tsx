db19e68da33f44b30c7a1c631774b746010c9a03
Suan Choi <siasiarhdwn@gmail.com>
2023-03-26 17:06:57 +0800

    docs: add favicon/schema diagram

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,12 @@ import { authOptions } from "@/pages/api/auth/[...nextauth]"
 import { redirect } from "next/navigation";
 import MyPosts from "./MyPosts";
 import styles from './page.module.scss';
+import { Metadata } from "next";
+
+export async function generateMetadata (): Promise<Metadata> {
+  const session = await getServerSession(authOptions);
+  return {title: `${session?.user?.name}'s dashboard`}
+};
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
